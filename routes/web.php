2f23ecc496582e662079c8b9dfb477f601e6e064
maheshadhikari-gla <Mahesh.Adhikari@glasgow.ac.uk>
2025-08-27 14:30:25 +0100

    use before require

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-require __DIR__ . '/sso-auth.php';
-
 use Illuminate\Support\Facades\Route;
+
+require __DIR__ . '/sso-auth.php';
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', \App\Livewire\HomePage::class)->name('home');
