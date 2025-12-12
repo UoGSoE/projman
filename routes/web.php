dfe5e9ca0d7b5ee765a40f7b41c8e300e48cec32
Ohffs <ohffsnotnow@gmail.com>
2025-12-12 15:01:10 +0000

    Rename routes from /projects to /work-packages

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,16 @@ require __DIR__.'/sso-auth.php';
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', \App\Livewire\HomePage::class)->name('home');
-    Route::get('/projects', \App\Livewire\ProjectList::class)->name('projects');
-    Route::get('/project/create', \App\Livewire\ProjectCreator::class)->name('project.create');
-    Route::get('/project/{project}', \App\Livewire\ProjectViewer::class)->name('project.show');
-    Route::get('/project/{project}/edit', \App\Livewire\ProjectEditor::class)->name('project.edit');
-    Route::get('/staff/heatmap', \App\Livewire\HeatMapViewer::class)->name('project.heatmap');
-
+    Route::get('/work-packages', \App\Livewire\ProjectList::class)->name('projects');
+    Route::get('/work-package/create', \App\Livewire\ProjectCreator::class)->name('project.create');
+    Route::get('/work-package/{project}', \App\Livewire\ProjectViewer::class)->name('project.show');
+    Route::get('/work-package/{project}/edit', \App\Livewire\ProjectEditor::class)->name('project.edit');
 
     Route::get('/profile', \App\Livewire\Profile::class)->name('profile');
 
     Route::middleware(['admin'])->group(function () {
         Route::get('/staff', \App\Livewire\UserList::class)->name('users.list');
+        Route::get('/staff/heatmap', \App\Livewire\HeatMapViewer::class)->name('project.heatmap');
         Route::get('/roles', \App\Livewire\RolesList::class)->name('roles.list');
         Route::get('/skills', \App\Livewire\SkillsManager::class)->name('skills.manage');
         Route::get('/user/{user}', \App\Livewire\UserViewer::class)->name('user.show');
